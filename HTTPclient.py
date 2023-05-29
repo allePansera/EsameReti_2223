c584999bd0f2951d1cf5e18db3d3cf752f28cb11
Alessandro Pansera <46775926+allePansera@users.noreply.github.com>
2023-05-29 09:12:43 +0200

    Add files via upload
    Socket programming

---
+++ HTTPclient.py
@@ -0,0 +1,10 @@
+import socket, sys
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 8081
+URL = sys.argv[1] if len(sys.argv) == 2 else Exception("Definire l'url da richiedere")
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    request = f"""GET {URL} HTTP/1.1\r\nHost: localhost\r\n\r\n"""
+    s.sendall(request.encode('utf-8'))
+    print("Risposta:\n\n{}".format(s.recv(1024).decode('utf-8')))
---
+++ SMTPclient.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import socket
+import os
+import sys
+import time
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 2525  # Port to connect to
+
+
+def expect_response(s: str, exp_code: str):
+    # non dovrebbero arrivarmi più righe, ma qualora mi arrivassero considero solo l'ultima.
+    data = s.splitlines()
+    for l in data:
+        print(l)
+    # data[last_line] -> response
+    rv = data[-1][0:3]
+    if rv != exp_code:
+        print('received: "%s" instead of "%s"' % (rv, exp_code))
+        sys.exit()
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    # Receive welcome message    
+    expect_response(s.recv(1024).decode('ascii'), '220')
+    # send HELO message    
+    s.sendall('HELO localhost\r\n'.encode('ascii'))
+    expect_response(s.recv(1024).decode('ascii'), '250')
+    # send MAIL envelope
+    s.sendall('MAIL FROM: <riccardo@localhost>\r\n'.encode('ascii'))
+    expect_response(s.recv(1024).decode('ascii'), '250')
+    s.sendall('RCPT TO: <riccardo@localhost>\r\n'.encode('ascii'))
+    expect_response(s.recv(1024).decode('ascii'), '250')
+    # send MAIL message (RFC822)
+    s.sendall('DATA\r\n'.encode('ascii'))
+    expect_response(s.recv(1024).decode('ascii'), '354')
+    s.sendall('From: riccardo <riccardo@localhost>\r\n' \
+              'To: riccardo <riccardo@localhost>\r\n' \
+              'Subject: prova\r\n\r\n' \
+              'Messaggio di prova\r\n.\r\n'.encode('ascii'))
+    expect_response(s.recv(1024).decode('ascii'), '250')
+    # Submit message
+    s.sendall('QUIT\r\n'.encode('ascii'))
+    expect_response(s.recv(1024).decode('ascii'), '221')
